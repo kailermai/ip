e035bf86df88a1ccc09c6027dac3ff866de333be
Kai Ler <133531148+kailermai@users.noreply.github.com>
2025-08-20 23:25:58 +0800

    Add echoing to user inputs
    Chatbot will echo the user command input back if
    the input is not bye, otherwise prints the
    goodbye message.

--- src/main/java/Angus.java
+++ src/main/java/Angus.java
@@ -1,16 +1,28 @@
+import java.util.Scanner;
+
 public class Angus {
     public static void main(String[] args) {
-        String horizontalLine = "____________________________________________________________\n";
+        String horizontalLine = "\t____________________________________________________________";
 
         String greetings = horizontalLine
-                + "Hello! I'm Angus\n"
-                + "What can I do for you?";
-
-        String goodbye = horizontalLine
-                + "Bye. Hope to see you again soon!\n"
+                + "\n\tHello! I'm Angus\n"
+                + "\tWhat can I do for you?\n"
                 + horizontalLine;
 
+        String goodbye = horizontalLine
+                + "\n\tBye. Hope to see you again soon!\n"
+                + horizontalLine;
+
+        Scanner input = new Scanner(System.in);
         System.out.println(greetings);
+        String command = input.nextLine();
+        while (!command.equals("bye")) {
+            String message = horizontalLine
+                    + "\n\t" + command + "\n"
+                    + horizontalLine;
+            System.out.println(message);
+            command = input.nextLine();
+        }
         System.out.println(goodbye);
     }   
 }
