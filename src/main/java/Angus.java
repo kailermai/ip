d86dc4d6e0a40360c456fb8df2c51eddea93124e
Kai Ler <133531148+kailermai@users.noreply.github.com>
2025-08-20 23:44:32 +0800

    Create helper function to format Angus response

--- src/main/java/Angus.java
+++ src/main/java/Angus.java
@@ -1,25 +1,27 @@
 import java.util.Scanner;
 
 public class Angus {
+    static String HORIZONTAL_LINE = "\t____________________________________________________________";
+    static String LINE_SEPARATOR = "\n\t";
+
+    public static String angusResponse(String text) {
+        return HORIZONTAL_LINE + LINE_SEPARATOR + text + "\n" + HORIZONTAL_LINE;
+    }
+
     public static void main(String[] args) {
-        String horizontalLine = "\t____________________________________________________________";
+        String greetingsText = "Hello! I'm Angus o_O" +
+                LINE_SEPARATOR +
+                "What can I do for you today?";
+        String greetings = angusResponse(greetingsText);
 
-        String greetings = horizontalLine
-                + "\n\tHello! I'm Angus o_O\n"
-                + "\tWhat can I do for you today?\n"
-                + horizontalLine;
-
-        String goodbye = horizontalLine
-                + "\n\tGoodbye. Hope to see you again soon!\n"
-                + horizontalLine;
+        String goodbyeText = "Goodbye. Hope to see you again soon!";
+        String goodbye = angusResponse(goodbyeText);
 
         Scanner input = new Scanner(System.in);
         System.out.println(greetings);
         String command = input.nextLine();
         while (!command.equals("bye")) {
-            String message = horizontalLine
-                    + "\n\t" + command + "\n"
-                    + horizontalLine;
+            String message = angusResponse(command);
             System.out.println(message);
             command = input.nextLine();
         }
