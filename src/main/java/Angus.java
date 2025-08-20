20f36fa1e402e827e02b9ab28c4cea604850918c
Kai Ler <133531148+kailermai@users.noreply.github.com>
2025-08-21 00:00:41 +0800

    Change logic for chatbot response
    Switch statements were incompatible with previous
    implementation of checking if the user response,
    so changed while loop to run while the running
    variable is true.

--- src/main/java/Angus.java
+++ src/main/java/Angus.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Angus {
     private final static String HORIZONTAL_LINE =
             "\t____________________________________________________________";
     private final static String LINE_SEPARATOR = "\n\t";
+
+    List<Task> taskList = new ArrayList<>();
 
     public static String angusResponse(String text) {
         return HORIZONTAL_LINE + LINE_SEPARATOR + text + "\n" + HORIZONTAL_LINE;
@@ -20,11 +24,25 @@ public class Angus {
 
         Scanner input = new Scanner(System.in);
         System.out.println(greetings);
-        String command = input.nextLine();
-        while (!command.equals("bye")) {
-            String message = angusResponse(command);
-            System.out.println(message);
+        boolean running = true;
+        String command;
+
+        while (running) {
             command = input.nextLine();
+
+            switch (command) {
+                case "bye":
+                    running = false;
+                    break;
+                case "list":
+
+                    break;
+
+                default:
+                    String message = angusResponse(command);
+                    System.out.println(message);
+                    break;
+            }
         }
         System.out.println(goodbye);
     }   
