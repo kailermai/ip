c1eccaa9c81a82c91b417d0d568fc898f941e58a
Kai Ler <133531148+kailermai@users.noreply.github.com>
2025-08-21 15:55:03 +0800

    Add implementation for deadline class

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,2 +1,18 @@
-public class Deadline {
+public class Deadline extends Task {
+    String endDate;
+
+    public Deadline(String description, String endDate) {
+        super(description);
+        this.endDate = endDate;
+    }
+
+    @Override
+    public String getStatusIcon() {
+        return "[D]" + super.getStatusIcon();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " (by:" + this.endDate + ")";
+    }
 }
