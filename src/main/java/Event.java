2ee112e2bdb2f7025fa3a1c454715cd7c6d8491f
Kai Ler <133531148+kailermai@users.noreply.github.com>
2025-08-21 15:35:17 +0800

    Add implementation for event class

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,2 +1,18 @@
-public class Event {
+public class Event extends Task {
+    String deadline;
+
+    public Event(String description, String deadline) {
+        super(description);
+        this.deadline = deadline;
+    }
+
+    @Override
+    public String getStatusIcon() {
+        return "[E]" + super.getStatusIcon();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + this.deadline;
+    }
 }
