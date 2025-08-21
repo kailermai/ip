0ba7d867fcb11854cef6c8fd4c7a6e7ef827f606
Kai Ler <133531148+kailermai@users.noreply.github.com>
2025-08-21 14:17:39 +0800

    Add ability to mark task as done

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,18 @@
 public class Task {
     private final String taskName;
+    private boolean isDone;
 
     public Task(String taskName) {
         this.taskName = taskName;
+        this.isDone = false;
     }
 
     public String getTaskName() {
         return this.taskName;
+    }
+
+    public boolean markDone() {
+        this.isDone = !this.isDone;
+        return this.isDone;
     }
 }
