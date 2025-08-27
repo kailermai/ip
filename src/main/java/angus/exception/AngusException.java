cc58892e3b0eaea2e3731fa385cf658a685ac67a
kailermai <133531148+kailermai@users.noreply.github.com>
2025-08-27 16:46:34 +0800

    Add JavaDoc comments to AngusException

--- src/main/java/angus/exception/AngusException.java
+++ src/main/java/angus/exception/AngusException.java
@@ -1,5 +1,8 @@
 package angus.exception;
 
+/**
+ * Represents an exception to be thrown by the chatbot application Angus
+ */
 public class AngusException extends Exception {
     public AngusException(String message) {
         super(message);
