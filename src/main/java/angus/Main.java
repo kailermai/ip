c7189997b85d51f5bd76e96dc68014fbed724391
kailermai <133531148+kailermai@users.noreply.github.com>
2025-09-03 15:17:57 +0800

    Add files from JavaFX tutorial

---
+++ src/main/java/angus/Main.java
@@ -0,0 +1,31 @@
+package angus;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * A GUI for Angus using FXML.
+ */
+public class Main extends Application {
+
+    private Angus angus = new Angus();
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/resources/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setAngus(angus);  // inject the Angus instance
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
